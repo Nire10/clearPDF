375782a21c78cf63043ef719e0107eedccc00912
Nire10 <erguthrie@gmail.com>
2015-04-28 11:47:08 -0700

    Update Doc.java
    blah.

--- Doc.java
+++ Doc.java
@@ -1,27 +1,30 @@
 public class Doc
 {
-    private String baseName;
     private int outType;
     private String workFolder;
+    private String sourceFolder;
     private int pageCount;
 
     //constructor
-    public Doc(String workfolder)
+    public Doc(String workfolder, String sourcefolder)
     {
         workFolder = workfolder;
+        sourceFolder = sourcefolder;
+        PreProcess prep = new PreProcess(workFolder);
+        ProcessStructure procstru = new ProcessStructure(workFolder);
+        PostProcess postp = new PostProcess();
     }
 
-    public void doDoc(String name)
+    public void doDoc(String basename)
     {
+        //TODO: copy basename.pdf?? from source folder into work folder
+        //copy (sourceFolder + "/" + basename + ".pdf") -> (workFolder + "/" + basename + ".pdf")
         //sends the doc, basename, and workfolder. returns an array(???) of words [Type?]
-        PreProcess prep = new PreProcess(workFolder);
         wordList = prep.preProcessDoc(baseName);
-
         //sends the words to structure the page. 
-        ProcessStructure procstru = new ProcessStructure(workFolder);
+        procstru.processStructureDoc(wordList);
         
         //sends the finished doc to be cleaned up. return value???
-        procstru.processStructureDoc(x);
-        PostProcess postp = new PostProcess();
+        
     }
 }
