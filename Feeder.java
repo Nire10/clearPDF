c023fe4e1a0bd27380e415d7a532a24ee0a07ad4
Nire10 <erguthrie@gmail.com>
2015-04-28 11:22:45 -0700

    Create Feeder.java
    Not at all done. I'm not really sure what is needed in this
    --Erin

---
+++ Feeder.java
@@ -0,0 +1,18 @@
+public class Feeder
+{
+    //variables: array of base names, pathnames/folders, etc
+    
+    public Feeder()
+    {
+        //TODO: go through folder and collect basenames, does other things idk. 
+        
+        //create a 'Doc' object.
+        Doc docthing = new Doc(workFolder);
+        /* send a basename/file to be done
+        for (itterate through basename array)
+        docthing.doDoc(basename);
+        */
+    }
+    
+    
+}
